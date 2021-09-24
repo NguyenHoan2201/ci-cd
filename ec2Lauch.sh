7389e92388fc1427c27135d4c04ddeb7cd9479cc
Hoan Nguyen <nvhoan14@gmail.com>
2021-09-24 09:26:55 +0700

    update

--- ec2Lauch.sh
+++ ec2Lauch.sh
@@ -3,6 +3,6 @@ sudo yum -y update
 sudo yum -y install ruby
 sudo yum -y install wget
 cd /home/ec2-user
-wget https://aws-codedeploy-us-east-1.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 sudo chmod +x ./install
 sudo ./install auto
--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
   })
 })
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(3000, () => console.log('server listening on port 3000'));
